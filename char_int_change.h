7748e493946094957d3d706ba13c2568295f3029
Yu-Chun1214 <yuchunlin0075@gmail.com>
2018-08-11 13:38:59 +0800

    remove trash

--- char_int_change.h
+++
@@ -1,6 +0,0 @@
-#ifndef __CHAR_INT_CHANGE_H__
-#define __CHAR_INT_CHANGE_H__
-
-int interger(char inter){
-    char digits[]
-}
--- test.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    cout<<"Test.cpp"<<endl;
-    system("python ./test.py");
-}
